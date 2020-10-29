72642ae88b29ba0982f8081acd8b2a11bea3c8fe
Valencia Richards <63933204+v-richards@users.noreply.github.com>
2020-10-28 21:09:00 -0400

    Complete Stage 3: Add functionality to: enter deadlines when adding tasks; show today's tasks; show this week's tasks; and show all tasks

--- To-Do List/task/todolist/todolist.py
+++ To-Do List/task/todolist/todolist.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime
-
+from datetime import datetime, timedelta
 
 # Connect to the database
 engine = create_engine("sqlite:///todo.db?check_same_thread=False")
@@ -14,16 +13,15 @@ Base = declarative_base()
 
 # Create the model class
 class Table(Base):
+	"""Table class that creates rows in the database"""
 
-    """Table class that creates rows in the database"""
+	__tablename__ = "task"
+	id = Column(Integer, primary_key=True)
+	task = Column(String, default="default_value")
+	deadline = Column(Date, default=datetime.today())
 
-    __tablename__ = "task"
-    id = Column(Integer, primary_key=True)
-    task = Column(String, default="default_value")
-    deadline = Column(Date, default=datetime.today())
-
-    def __repr__(self):
-        return self.task
+	def __repr__(self):
+		return self.task
 
 
 # Create a table in the database
@@ -48,41 +46,71 @@ rows = session.query(Table).all()
 
 
 def show_menu():
-    print("1) Today's tasks\n"
-          "2) Add task\n"
-          "0) Exit")
+	print("""\n1) Today's tasks
+2) Week's tasks
+3) All tasks
+4) Add task
+0) Exit""")
 
 
-def show_tasks():
-    tasks = session.query(Table).all()
-    if tasks:
-        print("\nToday:")
-        for task in tasks:
-            print(task)
-        print("")
-    else:
-        print("\nNothing to do!")
+def show_todays_tasks():
+	today = datetime.today()
+	print(f"\nToday {today.strftime('%d %b')}:")
+	tasks = session.query(Table).filter(Table.deadline == today.date()).all()
+	if tasks:
+		for index, task in enumerate(tasks):
+			print(f"{index + 1}. {task.task}")
+	else:
+		print("Nothing to do!")
+
+
+def show_this_weeks_tasks():
+	today = datetime.today()
+	for day in range(7):
+		date = today.date() + timedelta(day)
+		tasks = session.query(Table).filter(Table.deadline == date).all()
+		print(f"\n{date.strftime('%A %-d %b')}:")
+		if tasks:
+			for index, task in enumerate(tasks):
+				print(f"{index + 1}. {task.task}")
+		else:
+			print("Nothing to do!")
+
+
+def show_all_tasks():
+	tasks = session.query(Table).order_by(Table.deadline).all()
+	if tasks:
+		print()
+		for index, task in enumerate(tasks):
+			print(f"{index + 1}. {task.task}. {task.deadline.strftime('%-d %b')}")
+	else:
+		print("\nNothing to do!")
 
 
 def add_task():
-    user_input = input("\nEnter task\n")
-    new_task = Table(task=user_input)
-    session.add(new_task)
-    session.commit()
-    print("The task has been added!\n")
+	new_task = input("\nEnter task")
+	new_deadline = datetime.strptime(input("Enter deadline"), "%Y-%m-%d").date()
+	new_row = Table(task=new_task, deadline=new_deadline)
+	session.add(new_row)
+	session.commit()
+	print("The task has been added!")
 
 
 def main():
-    while True:
-        show_menu()
-        user_choice = input()
-        if user_choice == "1":
-            show_tasks()
-        elif user_choice == "2":
-            add_task()
-        elif user_choice == "0":
-            print("\nBye!")
-            break
+	while True:
+		show_menu()
+		user_choice = input()
+		if user_choice == "1":
+			show_todays_tasks()
+		if user_choice == "2":
+			show_this_weeks_tasks()
+		if user_choice == "3":
+			show_all_tasks()
+		elif user_choice == "4":
+			add_task()
+		elif user_choice == "0":
+			print("\nBye!")
+			break
 
 
 main()
